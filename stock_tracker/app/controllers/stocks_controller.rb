e8b6b9b0ed2c2511da2f0435b7c8042c731ff2e0
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-11-30 00:12:13 +0900

    add search validation & ajax search

--- stock_tracker/app/controllers/stocks_controller.rb
+++ stock_tracker/app/controllers/stocks_controller.rb
@@ -1,6 +1,33 @@
 class StocksController < ApplicationController
+  before_action :set_keyword
+  before_action :blank?
+
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    @stock = Stock.new_lookup(params[:stock])
+    if @stock.present?
+      respond_to do |format|
+        format.js { render partial: "users/result" }
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a valid symbol to search"
+        format.js { render partial: "users/result" }
+      end
+    end
+  end
+
+  private
+
+  def set_keyword
+    @keyword = params[:stock].presence || nil
+  end
+
+  def blank?
+    if @keyword.blank?
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: "users/result" }
+      end
+    end
   end
 end
