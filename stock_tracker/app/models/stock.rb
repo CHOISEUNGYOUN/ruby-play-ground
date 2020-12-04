e3e7f76de62fc5c63ed0f61a256e29e349ac2df7
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-12-04 23:19:45 +0900

    build a stock add func

--- stock_tracker/app/models/stock.rb
+++ stock_tracker/app/models/stock.rb
@@ -4,6 +4,8 @@ class Stock < ApplicationRecord
 
   validates :name, :ticker, presence: true
 
+  scope :grab_stock, ->(ticker) { where(ticker: ticker) }
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
