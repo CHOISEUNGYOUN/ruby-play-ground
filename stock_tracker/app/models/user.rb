2a716f2f7e5534d9dcdd1b6fba76b451241a2ba0
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-12-05 00:07:02 +0900

    add a no-more-than-10 restriction func

--- stock_tracker/app/models/user.rb
+++ stock_tracker/app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def already_tracked?(ticker_symbol)
+    stock = Stock.grab_stock(ticker_symbol).first
+    return false unless stock
+
+    stocks.where(id: stock).exists?
+  end
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def track_stock?(ticker_symbol)
+    under_stock_limit? && !already_tracked?(ticker_symbol)
+  end
 end
