6da5944ed9ff37f1ad5df2024dca549d4653d9db
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-12-07 22:35:40 +0900

    add a stock remove func & btn

--- stock_tracker/app/controllers/user_stocks_controller.rb
+++ stock_tracker/app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user: current_user, stock: stock).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio."
+    redirect_to my_portfolio_path
+  end
 end
