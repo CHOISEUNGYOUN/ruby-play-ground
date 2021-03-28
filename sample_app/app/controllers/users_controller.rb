021d5efeb855f979d3b3c148fabb79013b8c88aa
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-03-28 14:54:30 +0000

    wip with user page

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def new
   end
+  
+  def show
+  end
+  
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
