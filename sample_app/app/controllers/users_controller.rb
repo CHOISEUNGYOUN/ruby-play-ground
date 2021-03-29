2a3e7176b55050feec790fb7226bfa81885464d1
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-03-29 14:53:14 +0000

    working on signup page

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -2,13 +2,27 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
   def new
+    @user = User.new
   end
   
   def show
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render :new
+    end
+  end
+  
   private
     def set_user
       @user = User.find(params[:id])
+    end
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
