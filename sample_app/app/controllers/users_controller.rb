bcb5958805197a51d36a05f3796cb524138bd617
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-04-28 14:55:03 +0000

    add authorization for editing user

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update]
   
   def new
     @user = User.new
@@ -40,5 +41,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_path
+      end
     end
 end
